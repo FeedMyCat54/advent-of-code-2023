c5d0930a80e051dcc2debf25a783508dbf9dca7e
Konstantinos Angelou <konstantangelou@gmail.com>
2023-12-01 14:05:07 +0200

    Add day 1 part 1

--- bin/advent_of_code.dart
+++ bin/advent_of_code.dart
@@ -1,5 +1,24 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
 import 'package:advent_of_code/advent_of_code.dart' as advent_of_code;
 
-void main(List<String> arguments) {
-  print('Hello world: ${advent_of_code.calculate()}!');
+void main(List<String> arguments) async {
+  final file = File('bin\\input.txt');
+  Stream<String> lines = file
+      .openRead()
+      .transform(utf8.decoder) // Decode bytes to UTF-8.
+      .transform(LineSplitter()); // Convert stream to individual lines.
+
+  int sum = 0;
+
+  try {
+    await for (var line in lines) {
+      sum += advent_of_code.calculateLineNumber(line: line);
+    }
+    print('The value is: $sum');
+  } catch (e) {
+    print('Error: $e');
+  }
 }
--- lib/advent_of_code.dart
+++ lib/advent_of_code.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+int calculateLineNumber({required String line}) {
+  RegExp exp = RegExp(r'\d');
+  Iterable<Match> matches = exp.allMatches(line);
+
+  String first = matches.first[0]!;
+  String last = matches.last[0]!;
+
+  return int.parse(first + last);
 }

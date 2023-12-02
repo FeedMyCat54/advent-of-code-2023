bf5f33cede0c96908af12521345fc2288497789d
Konstantinos Angelou <konstantangelou@gmail.com>
2023-12-02 22:50:30 +0200

    Add part 2

--- bin/advent_of_code.dart
+++ bin/advent_of_code.dart
@@ -1,39 +1,31 @@
 import 'dart:io';
-import 'dart:convert';
-import 'dart:async';
-import 'dart:developer';
 
 import 'package:advent_of_code/advent_of_code.dart';
 
-void main(List<String> arguments) async {
+void main(List<String> arguments) {
   const redMax = 12;
   const greenMax = 13;
   const blueMax = 14;
   int sum = 0;
+  int power = 0;
 
   final file = File('bin/input.txt');
-  Stream<String> lines = file
-      .openRead()
-      .transform(utf8.decoder) // Decode bytes to UTF-8.
-      .transform(LineSplitter()); // Convert stream to individual lines.
+  List<String> lines = file.readAsLinesSync();
 
-  List<Game> gameSums = [];
   try {
-    await for (var line in lines) {
-      gameSums.add(calculateGameCubes(line));
+    for (var line in lines) {
+      Game result = calculateGameCubes(line);
+      if (result.blues <= blueMax &&
+          result.reds <= redMax &&
+          result.greens <= greenMax) {
+        sum += int.parse(result.id);
+      }
+
+      power += result.blues * result.reds * result.greens;
     }
   } catch (e) {
     print('Error: $e');
   }
-
-  for (var result in gameSums) {
-    inspect(result);
-    if (result.blueSum <= blueMax &&
-        result.redSum <= redMax &&
-        result.greenSum <= greenMax) {
-      sum += int.parse(result.id);
-    }
-  }
-
   print('The sum of the valid ids is: $sum');
+  print('The sum of the powers is: $power');
 }
--- lib/advent_of_code.dart
+++ lib/advent_of_code.dart
@@ -5,7 +5,6 @@ Game calculateGameCubes(String line) {
 
   final ([game, cubes]) = line.split(':');
   final ([_, id]) = game.split(' ');
-  print(id);
 
   final pulls = cubes.split(';');
   for (var pull in pulls) {
@@ -13,34 +12,38 @@ Game calculateGameCubes(String line) {
     for (var cube in splitPull) {
       cube = cube.trim();
       final ([quantity, color]) = cube.split(' ');
-      // print('$quantity $color');
       switch (color) {
         case 'blue':
-          blue += int.parse(quantity);
+          if (int.parse(quantity) > blue) {
+            blue = int.parse(quantity);
+          }
           break;
         case 'green':
-          green += int.parse(quantity);
+          if (int.parse(quantity) > green) {
+            green = int.parse(quantity);
+          }
           break;
         case 'red':
-          red += int.parse(quantity);
+          if (int.parse(quantity) > red) {
+            red = int.parse(quantity);
+          }
           break;
       }
     }
   }
-  print('$blue $green $red');
-  return Game(id: id, blueSum: blue, greenSum: green, redSum: red);
+  return Game(id: id, blues: blue, greens: green, reds: red);
 }
 
 class Game {
   final String id;
-  final int blueSum;
-  final int greenSum;
-  final int redSum;
+  final int blues;
+  final int greens;
+  final int reds;
 
   const Game({
     required this.id,
-    required this.blueSum,
-    required this.greenSum,
-    required this.redSum,
+    required this.blues,
+    required this.greens,
+    required this.reds,
   });
 }

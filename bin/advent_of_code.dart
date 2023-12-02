8b4106bb6e4f4d182344a0af0ed6af9d5916097a
Konstantinos Angelou <konstantangelou@gmail.com>
2023-12-02 22:13:24 +0200

    checkpoint

--- bin/advent_of_code.dart
+++ bin/advent_of_code.dart
@@ -1,5 +1,39 @@
-import 'package:advent_of_code/advent_of_code.dart' as advent_of_code;
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+import 'dart:developer';
 
-void main(List<String> arguments) {
-  print('Hello world: ${advent_of_code.calculate()}!');
+import 'package:advent_of_code/advent_of_code.dart';
+
+void main(List<String> arguments) async {
+  const redMax = 12;
+  const greenMax = 13;
+  const blueMax = 14;
+  int sum = 0;
+
+  final file = File('bin/input.txt');
+  Stream<String> lines = file
+      .openRead()
+      .transform(utf8.decoder) // Decode bytes to UTF-8.
+      .transform(LineSplitter()); // Convert stream to individual lines.
+
+  List<Game> gameSums = [];
+  try {
+    await for (var line in lines) {
+      gameSums.add(calculateGameCubes(line));
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+
+  for (var result in gameSums) {
+    inspect(result);
+    if (result.blueSum <= blueMax &&
+        result.redSum <= redMax &&
+        result.greenSum <= greenMax) {
+      sum += int.parse(result.id);
+    }
+  }
+
+  print('The sum of the valid ids is: $sum');
 }
--- lib/advent_of_code.dart
+++ lib/advent_of_code.dart
@@ -1,3 +1,46 @@
-int calculate() {
-  return 6 * 7;
+Game calculateGameCubes(String line) {
+  int blue = 0;
+  int green = 0;
+  int red = 0;
+
+  final ([game, cubes]) = line.split(':');
+  final ([_, id]) = game.split(' ');
+  print(id);
+
+  final pulls = cubes.split(';');
+  for (var pull in pulls) {
+    final splitPull = pull.split(',');
+    for (var cube in splitPull) {
+      cube = cube.trim();
+      final ([quantity, color]) = cube.split(' ');
+      // print('$quantity $color');
+      switch (color) {
+        case 'blue':
+          blue += int.parse(quantity);
+          break;
+        case 'green':
+          green += int.parse(quantity);
+          break;
+        case 'red':
+          red += int.parse(quantity);
+          break;
+      }
+    }
+  }
+  print('$blue $green $red');
+  return Game(id: id, blueSum: blue, greenSum: green, redSum: red);
+}
+
+class Game {
+  final String id;
+  final int blueSum;
+  final int greenSum;
+  final int redSum;
+
+  const Game({
+    required this.id,
+    required this.blueSum,
+    required this.greenSum,
+    required this.redSum,
+  });
 }
